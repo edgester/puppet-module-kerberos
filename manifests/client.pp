472a3b5101b5fd7eeb4a014b249eb063ad51b4d6
Greg Anderson <greg.1.anderson@greenknowe.org>
2014-03-13 17:06:13 -0700

    If the user does not provide a domain_realm hash, then use '.realm = REALM' as a default.  Domain is generated by lowercasing the realm and prepending a '.'.

--- manifests/client.pp
+++ manifests/client.pp
@@ -11,6 +11,17 @@ class kerberos::client($realm = 'EXAMPLE.COM', $domain_realm = {}, $kdc = [], $a
 
   include kerberos::base
 
+  # Provide default content for domain_realm if the user did not
+  # specify anything.
+  if empty($domain_realm) {
+    $realm_in_lowercase = downcase($realm)
+    $default_domain = ".${realm_in_lowercase}"
+    $domain_realm_list = { "$default_domain" => "$realm" }
+  }
+  else {
+    $domain_realm_list = $domain_realm
+  }
+
   package { 'krb5-client-packages' :
     ensure => present,
     name   => $kerberos::params::client_packages,
