e920d33684d4dfd151e0816f69020472693b9925
Jason Edgecombe <jason@rampaginggeek.com>
2015-02-21 19:21:37 -0500

    Make a simple rspec test work

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 describe 'kerberos' do
-
+  
+  let(:facts) { {:operatingsystem => 'Debian'} }
   context 'with defaults for all parameters' do
     it { should contain_class('kerberos') }
   end
