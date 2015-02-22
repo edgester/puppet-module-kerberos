cf685c271ebea43c63b4905be5327cee7d10ad33
Jason Edgecombe <jason@rampaginggeek.com>
2015-02-22 12:58:40 -0500

    STYLE: Fix most warnings from 'rake lint' in manifests/ktadd.pp

--- manifests/ktadd.pp
+++ manifests/ktadd.pp
@@ -18,20 +18,20 @@ define kerberos::ktadd(
   $kadmin_tries = undef, $kadmin_try_sleep = undef,
 ) {
   if $local {
-    $kadmin = "kadmin.local"
+    $kadmin = 'kadmin.local'
     Package['krb5-kadmind-server-packages'] -> Exec["ktadd_${keytab}_${principal}"]
     Exec['create_krb5kdc_principal'] -> Exec["ktadd_${keytab}_${principal}"]
   } else {
-    $kadmin = "kadmin"
+    $kadmin = 'kadmin'
 
     $ccache_par = $kadmin_ccache ? {
-      undef   => "",
-      default => "-c '$kadmin_ccache'"
+      undef   => '',
+      default => "-c '${kadmin_ccache}'"
     }
 
     $keytab_par = $kadmin_keytab ? {
-      undef   => "",
-      default => "-k '$kadmin_keytab'"
+      undef   => '',
+      default => "-k '${kadmin_keytab}'"
     }
 
     Package['krb5-client-packages'] -> Exec["ktadd_${keytab}_${principal}"]
@@ -45,11 +45,11 @@ define kerberos::ktadd(
   }
 
   exec { "ktadd_${keytab}_${principal}":
-    command => "$kadmin $ccache_par $keytab_par -q 'ktadd -k $keytab $principal'",
-    unless  => $unless,
-    path    => [ "/bin", "/usr/bin", "/usr/bin", "/usr/sbin" ],
-    require => File[$keytab],
-    tries => $kadmin_tries,
+    command   => "${kadmin} ${ccache_par} ${keytab_par} -q 'ktadd -k ${keytab} ${principal}'",
+    unless    => $unless,
+    path      => [ '/bin', '/usr/bin', '/usr/bin', '/usr/sbin' ],
+    require   => File[$keytab],
+    tries     => $kadmin_tries,
     try_sleep => $kadmin_try_sleep,
   }
 }
