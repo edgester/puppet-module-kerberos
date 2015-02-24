604961aa356c1ed77bd1039d8ea3ec4af55038f7
Jason Edgecombe <jason@rampaginggeek.com>
2015-02-23 22:06:47 -0500

    TESTS: Check for kerberos::client class when client=>true

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,4 +7,13 @@ describe 'kerberos' do
     it { should_not contain_class('kerberos::client') }
     it { should_not contain_class('kerberos::kdc::master') }
   end
+
+  context 'with client=true' do
+    let(:facts) { {:operatingsystem => 'Debian'} }
+    let(:params) { { :client => true } }
+    it { should contain_class('kerberos') }
+    it { should contain_class('kerberos::client') }
+    it { should_not contain_class('kerberos::kdc::master') }
+  end
+
 end
